d5f336abe702cf79767b3405da9a70ee20b48f9d
Maxime Ripard <maxime.ripard@free-electrons.com>
2016-11-29 11:10:04 +0100

    r6p0: Revert our changes and switch to a patch based solution
    Since we want to support r6p2 too, and that the patches to apply are the
    same in both r6p0 and r6p2, remove the changes we made to the code, create
    patches from them, and apply those patches as part of the build process.
    
    Signed-off-by: Maxime Ripard <maxime.ripard@free-electrons.com>

--- build.sh
+++ build.sh
@@ -1,5 +1,51 @@
-#!/bin/sh
+#!/bin/bash
 
-DRIVER_DIR=$(pwd)/driver/src/devicedrv/mali/
-USING_UMP=0 BUILD=release USING_PROFILING=0 MALI_PLATFORM=sunxi USING_DVFS=0 make -C $DRIVER_DIR
-cp $DRIVER_DIR/mali.ko .
+RELEASE=r6p0
+
+apply_patches() {
+    pushd $2
+
+    for patch in $1/*.patch;
+    do
+	patch $3 -p1 < $patch
+    done
+
+    popd
+}
+
+build_driver() {
+    local driver_dir=$(pwd)/$RELEASE/src/devicedrv/mali/
+
+    USING_UMP=0 BUILD=release USING_PROFILING=0 MALI_PLATFORM=sunxi \
+	     USING_DVFS=0 make -C $driver_dir
+
+    cp $driver_dir/mali.ko .
+}
+
+while getopts "r:aub" opt
+do
+    case $opt in
+	a)
+	    echo "applying patches"
+	    apply_patches $(pwd)/patches $RELEASE
+	    ;;
+	b)
+	    echo "building..."
+	    apply_patches $(pwd)/patches $RELEASE
+	    build_driver $RELEASE
+	    ;;
+	r)
+	    RELEASE=$OPTARG
+	    ;;
+	u)
+	    echo "unapplying patches"
+	    apply_patches $(pwd)/patches $RELEASE -R
+	    ;;
+	\?)
+	    echo "invalid option -$OPTARG"
+	    exit 1
+	    ;;
+    esac
+done
+
+exit 0
--- r6p0/src/devicedrv/mali/linux/mali_memory_os_alloc.c
+++ r6p0/src/devicedrv/mali/linux/mali_memory_os_alloc.c
@@ -27,12 +27,8 @@
 #define MALI_OS_MEMORY_POOL_TRIM_JIFFIES (10 * CONFIG_HZ) /* Default to 10s */
 
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(3, 8, 0)
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 8, 0)
-static unsigned long dma_attrs_wc;
-#else
 /* Write combine dma_attrs */
 static DEFINE_DMA_ATTRS(dma_attrs_wc);
-#endif
 #endif
 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(3, 0, 0)
@@ -508,11 +504,7 @@ _mali_osk_errcode_t mali_mem_os_get_table_page(mali_dma_addr *phys, mali_io_addr
 	spin_unlock(&mali_mem_page_table_page_pool.lock);
 
 	if (_MALI_OSK_ERR_OK != ret) {
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 8, 0)
-		*mapping = dma_alloc_attrs(&mali_platform_device->dev,
-					   _MALI_OSK_MALI_PAGE_SIZE, &tmp_phys,
-					   GFP_KERNEL, dma_attrs_wc);
-#elif LINUX_VERSION_CODE >= KERNEL_VERSION(3, 8, 0)
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(3, 8, 0)
 		*mapping = dma_alloc_attrs(&mali_platform_device->dev,
 					   _MALI_OSK_MALI_PAGE_SIZE, &tmp_phys,
 					   GFP_KERNEL, &dma_attrs_wc);
@@ -551,11 +543,7 @@ void mali_mem_os_release_table_page(mali_dma_addr phys, void *virt)
 	} else {
 		spin_unlock(&mali_mem_page_table_page_pool.lock);
 
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 8, 0)
-		dma_free_attrs(&mali_platform_device->dev,
-			       _MALI_OSK_MALI_PAGE_SIZE, virt, phys,
-			       dma_attrs_wc);
-#elif LINUX_VERSION_CODE >= KERNEL_VERSION(3, 8, 0)
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(3, 8, 0)
 		dma_free_attrs(&mali_platform_device->dev,
 			       _MALI_OSK_MALI_PAGE_SIZE, virt, phys,
 			       &dma_attrs_wc);
@@ -611,10 +599,7 @@ static void mali_mem_os_page_table_pool_free(size_t nr_to_free)
 
 	/* After releasing the spinlock: free the pages we removed from the pool. */
 	for (i = 0; i < nr_to_free; i++) {
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 8, 0)
-		dma_free_attrs(&mali_platform_device->dev, _MALI_OSK_MALI_PAGE_SIZE,
-			       virt_arr[i], (dma_addr_t)phys_arr[i], dma_attrs_wc);
-#elif LINUX_VERSION_CODE >= KERNEL_VERSION(3, 8, 0)
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(3, 8, 0)
 		dma_free_attrs(&mali_platform_device->dev, _MALI_OSK_MALI_PAGE_SIZE,
 			       virt_arr[i], (dma_addr_t)phys_arr[i], &dma_attrs_wc);
 #else
@@ -765,9 +750,7 @@ _mali_osk_errcode_t mali_mem_os_init(void)
 		return _MALI_OSK_ERR_NOMEM;
 	}
 
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 8, 0)
-	dma_attrs_wc = DMA_ATTR_WRITE_COMBINE;
-#elif LINUX_VERSION_CODE >= KERNEL_VERSION(3, 8, 0)
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(3, 8, 0)
 	dma_set_attr(DMA_ATTR_WRITE_COMBINE, &dma_attrs_wc);
 #endif
 
