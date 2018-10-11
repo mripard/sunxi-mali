efb31c192d683516e6191c15e226179690259646
Ali Fırat ARI <afari@gtu.edu.tr>
2018-10-11 20:14:24 +0300

    Update mali_osk_time.c

--- r6p2/src/devicedrv/mali/linux/mali_osk_time.c
+++ r6p2/src/devicedrv/mali/linux/mali_osk_time.c
@@ -54,6 +54,6 @@ u64 _mali_osk_time_get_ns(void)
 u64 _mali_osk_boot_time_get_ns(void)
 {
 	struct timespec tsval;
-	getboottime(&tsval);
+	tsval = ktime_to_timespec(ktime_get_boottime());
 	return (u64)timespec_to_ns(&tsval);
 }
